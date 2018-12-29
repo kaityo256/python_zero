db655943b902182a1517df8df6f66f10e9862a39
H. Watanabe <kaityo@users.sourceforge.jp>
2018-12-29 15:43:34 +0900

    fixed bug

--- samples/ast/parse.py
+++ samples/ast/parse.py
@@ -4,26 +4,15 @@ import sys
 from graphviz import Digraph
 
 
-def visit(node, parent, d, s):
-    if isinstance(node, ast.Load):
-        return
-    d[node] = len(d)
-    if parent:
-        s.add((parent, node))
-    for i in ast.iter_child_nodes(node):
-        visit(i, node, d, s)
-
-
-def draw(s, d):
-    g = Digraph(format="png")
-    for node in d:
-        name = str(type(node).__name__)
-        g.node(str(d[node]), name)
-    for node, parent in s:
-        node_id = d[node]
-        parent_id = d[parent]
-        g.edge(str(parent_id), str(node_id))
-    g.render("test")
+def visit(node, nodes, pindex, g):
+    name = str(type(node).__name__)
+    index = len(nodes)
+    nodes.append(index)
+    g.node(str(index), name)
+    if index != pindex:
+        g.edge(str(index), str(pindex))
+    for n in ast.iter_child_nodes(node):
+        visit(n, nodes, index, g)
 
 
 filename = sys.argv[0]
@@ -31,8 +20,7 @@ if len(sys.argv) > 1:
     filename = sys.argv[1]
 
 with open(filename) as f:
-    d = {}
-    s = set()
+    graph = Digraph(format="png")
     tree = ast.parse(f.read())
-    visit(tree, None, d, s)
-    draw(s, d)
+    visit(tree, [], 0, graph)
+    graph.render("test")
