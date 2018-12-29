5372de60d64767a63d2ae97d769d8d2dd87bd4ba
H. Watanabe <kaityo@users.sourceforge.jp>
2018-12-29 18:58:04 +0900

    updates fib.py

--- samples/fib/fib.py
+++ samples/fib/fib.py
@@ -1,11 +1,8 @@
-
-
 def fib(k):
     if k in (0, 1):
         return k
     return fib(k-1) + fib(k-2)
 
-# %time fib(34)
 
 def fib_memo(k, h):
     if k in (0, 1):
@@ -15,5 +12,26 @@ def fib_memo(k, h):
     h[k] = fib_memo(k-1, h) + fib_memo(k-2, h)
     return h[k]
 
-# h = {}
-# %time fib_memo(34)
+
+def fib_linear(n):
+    if n in (0, 1):
+        return n
+    f1 = 0
+    f2 = 1
+    fn = 0
+    for _ in range(n-1):
+        fn = f1+f2
+        f1 = f2
+        f2 = fn
+    return fn
+
+
+def test():
+    n = 25
+    print(fib(n))
+    h = {}
+    print(fib_memo(n, h))
+    print(fib_linear(n))
+
+
+test()
