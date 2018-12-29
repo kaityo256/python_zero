06c07c6b770e5702da9c15661e4c42d0f230afb4
H. Watanabe <kaityo@users.sourceforge.jp>
2018-12-29 19:12:23 +0900

    adds fib_graph.py

---
+++ samples/fib/fib_graph.py
@@ -0,0 +1,23 @@
+from graphviz import Digraph
+
+def fib(n, parent, nodes, g):
+    index = len(nodes)
+    nodes.append(index)
+    if n == 1:
+        g.node(str(index), str(n), color="red")
+    else:
+        g.node(str(index), str(n))
+
+    if parent is not None:
+        g.edge(str(index), str(parent))
+    if n in (0, 1):
+        return
+    fib(n-1, index, nodes, g)
+    fib(n-2, index, nodes, g)
+
+def test():
+    g = Digraph(format="png")
+    fib(6, None, [], g)
+    g.render("test")
+
+test()
