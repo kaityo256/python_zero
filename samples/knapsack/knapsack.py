f49988e29a50ba7be65b4ecc941b836fbba106a2
H. Watanabe <kaityo@users.sourceforge.jp>
2019-12-06 20:22:55 +0900

    updates slides

--- samples/knapsack/knapsack.py
+++ samples/knapsack/knapsack.py
@@ -1,13 +1,14 @@
 from graphviz import Digraph
 
-#data = [("A", 5), ("B", 4), ("C", 3), ("D", 3)]
 
 
 g = Digraph(format='png')
 
 
-def func(data, mydata, pindex, nodes):
-    name = str(mydata)
+def func(data, mydata, pindex, nodes, total):
+    if total > 10:
+        return
+    name = str(mydata) + str(total)
     index = len(nodes)
     nodes.append(index)
     g.node(str(index), name)
@@ -18,12 +19,15 @@ def func(data, mydata, pindex, nodes):
     a = data.pop(0)
     for add in (True, False):
         m = mydata.copy()
+        mytotal = total
         if add:
-            m.append(a)
-        func(data.copy(), m, index, nodes)
+            m.append(a[0])
+            mytotal = mytotal + a[1]
+        func(data.copy(), m, index, nodes, mytotal)
 
 
-func([1, 2, 3, 4], [], 0, [])
+d = [("A", 5), ("B", 4), ("C", 3), ("D", 3)]
+func(d, [], 0, [], 0)
 
-# g.graph_attr.update(size="10,10")
+g.graph_attr.update(ratio="0.6")
 g.render("test")
