640fdcb3cb05eb1f5e64f3bb67fecc0a48c06cf7
H. Watanabe <kaityo@users.sourceforge.jp>
2020-05-17 00:12:01 +0900

    updates dcgan

--- ml/dcgan.py
+++ ml/dcgan.py
@@ -131,7 +131,7 @@ def generate_and_save_images(model, epoch, test_input):
         plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
         plt.axis('off')
 
-    filename = 'image_at_epoch_{:04d}.png'.format(epoch)
+    filename = 'img{:04d}.png'.format(epoch)
     plt.savefig(filename)
     plt.show()
 
