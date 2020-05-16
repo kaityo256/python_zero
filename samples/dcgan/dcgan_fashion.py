096bd547dbf59d79a287b567553ad6de0b923936
H. Watanabe <kaityo@users.sourceforge.jp>
2020-05-16 17:23:57 +0900

    updates

--- samples/dcgan/dcgan_fashion.py
+++ samples/dcgan/dcgan_fashion.py
@@ -1,5 +1,3 @@
-
-import os
 import time
 
 from IPython import display
@@ -49,15 +47,6 @@ def make_generator_model():
     return model
 
 
-generator = make_generator_model()
-
-noise = tf.random.normal([1, 100])
-generated_image = generator(noise, training=False)
-
-plt.imsave("test.png", generated_image[0, :, :, 0], cmap='gray')
-#plt.imshow(generated_image[0, :, :, 0], cmap='gray')
-
-
 def make_discriminator_model():
     model = tf.keras.Sequential()
     model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
@@ -75,13 +64,6 @@ def make_discriminator_model():
     return model
 
 
-discriminator = make_discriminator_model()
-decision = discriminator(generated_image)
-print(decision)
-
-cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-
-
 def discriminator_loss(real_output, fake_output):
     real_loss = cross_entropy(tf.ones_like(real_output), real_output)
     fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
@@ -91,17 +73,6 @@ def discriminator_loss(real_output, fake_output):
 
 def generator_loss(fake_output):
     return cross_entropy(tf.ones_like(fake_output), fake_output)
-
-
-generator_optimizer = tf.keras.optimizers.Adam(1e-4)
-discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
-
-checkpoint_dir = './training_checkpoints'
-checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
-checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-                                 discriminator_optimizer=discriminator_optimizer,
-                                 generator=generator,
-                                 discriminator=discriminator)
 
 
 @tf.function
@@ -141,11 +112,6 @@ def train(dataset, epochs=50, batch_size=256):
         # Produce images for the GIF as we go
         display.clear_output(wait=True)
         generate_and_save_images(generator, epoch + 1, seed)
-
-        # Save the model every 15 epochs
-        if (epoch + 1) % 15 == 0:
-            checkpoint.save(file_prefix=checkpoint_prefix)
-
         print('Time for epoch {} is {} sec'.format(
             epoch + 1, time.time() - start))
 
@@ -167,5 +133,10 @@ def generate_and_save_images(model, epoch, test_input):
     plt.show()
 
 
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+generator = make_generator_model()
+discriminator = make_discriminator_model()
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 train_dataset = get_dataset()
 train(train_dataset)
