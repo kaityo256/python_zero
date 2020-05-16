603a2c18c6e6f6bb54e50ddcd7b7e58c1334f300
H. Watanabe <kaityo@users.sourceforge.jp>
2020-05-16 17:10:29 +0900

    adds dcgan

---
+++ samples/dcgan/dcgan_fashion.py
@@ -0,0 +1,171 @@
+
+import os
+import time
+
+from IPython import display
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras import layers
+
+
+def get_dataset(batch_size=256):
+    fashion_mnist = tf.keras.datasets.fashion_mnist
+    (train_images, _), _ = fashion_mnist.load_data()
+    train_images = train_images.reshape(
+        train_images.shape[0], 28, 28, 1).astype('float32')
+    train_images = (train_images - 127.5) / 127.5
+    buffer_size = train_images.shape[0]
+    dataset = tf.data.Dataset.from_tensor_slices(
+        train_images).shuffle(buffer_size).batch(batch_size)
+    return dataset
+
+
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(100,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Reshape((7, 7, 256)))
+    # Note: None is the batch size
+    assert model.output_shape == (None, 7, 7, 256)
+
+    model.add(layers.Conv2DTranspose(
+        128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 128)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(
+        64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 14, 14, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+
+    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2),
+                                     padding='same', use_bias=False, activation='tanh'))
+    assert model.output_shape == (None, 28, 28, 1)
+
+    return model
+
+
+generator = make_generator_model()
+
+noise = tf.random.normal([1, 100])
+generated_image = generator(noise, training=False)
+
+plt.imsave("test.png", generated_image[0, :, :, 0], cmap='gray')
+#plt.imshow(generated_image[0, :, :, 0], cmap='gray')
+
+
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+                            input_shape=[28, 28, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
+
+
+discriminator = make_discriminator_model()
+decision = discriminator(generated_image)
+print(decision)
+
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
+
+
+@tf.function
+def train_step(images, batch_size, noise_dim):
+    noise = tf.random.normal([batch_size, noise_dim])
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise, training=True)
+
+        real_output = discriminator(images, training=True)
+        fake_output = discriminator(generated_images, training=True)
+
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(
+        gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(
+        disc_loss, discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(
+        zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(
+        zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+
+def train(dataset, epochs=50, batch_size=256):
+    noise_dim = 100
+    seed = tf.random.normal([16, noise_dim])
+    for epoch in range(epochs):
+        print(epoch)
+        start = time.time()
+
+        for image_batch in dataset:
+            train_step(image_batch, batch_size, noise_dim)
+
+        # Produce images for the GIF as we go
+        display.clear_output(wait=True)
+        generate_and_save_images(generator, epoch + 1, seed)
+
+        # Save the model every 15 epochs
+        if (epoch + 1) % 15 == 0:
+            checkpoint.save(file_prefix=checkpoint_prefix)
+
+        print('Time for epoch {} is {} sec'.format(
+            epoch + 1, time.time() - start))
+
+    # Generate after the final epoch
+    display.clear_output(wait=True)
+    generate_and_save_images(generator, epochs, seed)
+
+
+def generate_and_save_images(model, epoch, test_input):
+    predictions = model(test_input, training=False)
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i + 1)
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.axis('off')
+
+    filename = 'image_at_epoch_{:04d}.png'.format(epoch)
+    plt.savefig(filename)
+    plt.show()
+
+
+train_dataset = get_dataset()
+train(train_dataset)
